c67aca6b02307b9e896de920055b682cf2203721
Jan Miguel Carangan <jami.carangan@gmail.com>
2017-09-23 18:47:35 +0800

    cards layout complete. todo: add compare functionality

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <selection></selection>
     <ticket-types></ticket-types>
+    <cards></cards>
   </div>
 </template>
 
 <script>
 import Selection from './components/Selection'
 import TicketTypes from './components/TicketTypes'
+import Cards from './components/Cards'
 
 export default {
   name: 'app',
   components: {
     Selection,
-    TicketTypes
+    TicketTypes,
+    Cards
   }
 }
 </script>
---
+++ src/components/Cards.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-6 col-lg-3 align-self-center" v-for="(card, index) in cards" :key="index">
+        <div class="card text-center bg-gray mb-5 not-rounded has-shadow">
+          <div class="card-body text-active">
+            <div v-if="card.isPopular" class="h4 text-danger text-uppercase mt-2 mb-0">most popular</div>
+            <div class="title text-center">{{ card.title }}</div>
+            <div class="display-3 font-weight-bold">&euro;{{ card.price}}</div>
+            <div v-if="card.savings" class="savings">Save &euro;{{ card.savings }}</div>
+            <div v-else-if="card.deadline" class="savings">&nbsp;</div>
+            <div v-if="card.deadline">Until {{ card.deadline }}</div>
+            <div v-else>
+              <div class="text-muted small">Do you have a promo code?</div>
+                <input type="text" class="form-control-sm my-1" placeholder="Type it here & pick a day">
+                <ul class="list-unstyled list-inline custom-input">
+                  <li class="list-inline-item">
+                    <input id="day1" class="form-check-input" type="radio" name="day" value="option1">
+                    <label for="day1" class="form-check-label">DAY 1</label>
+                  </li>
+                  <li class="list-inline-item">
+                    <input id="day2" class="form-check-input" type="radio" name="day" value="option2">
+                    <label for="day2" class="form-check-label">DAY 2</label>
+                  </li>
+                  <li class="list-inline-item">
+                    <input id="day3" class="form-check-input" type="radio" name="day" value="option3">
+                    <label for="day3" class="form-check-label">BOTH</label>
+                  </li>
+                </ul>
+            </div>
+          </div>
+          <button class="btn btn-block text-uppercase not-rounded"
+          :class="{'btn-danger' : card.isPopular, 'btn-active': !card.isPopular}">
+            book now
+          </button>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+let benefits = [
+  'Cold-pressed poke thundercats brooklyn chillware chartreuse',
+  'Craft beer 3 moon tbh hoodie',
+  'YOLO synth hammock',
+  'Distillery aesthetic VHS affogato gentrify bespoke',
+  'Chia readymade schlitz brunch yuccie echo park'
+]
+export default {
+  name: 'cards',
+  data () {
+    return {
+      cards: [
+        {
+          isPopular: false,
+          title: 'summer saver',
+          price: 1595,
+          savings: 300,
+          deadline: 'August 31',
+          benefits: benefits
+        },
+        {
+          isPopular: true,
+          title: 'expo only',
+          price: 299,
+          savings: null,
+          deadline: 'October',
+          benefits: benefits
+        },
+        {
+          isPopular: false,
+          title: 'public & govermental sector',
+          price: 1295,
+          savings: '300 from the Summer Saver',
+          deadline: 'October',
+          benefits: benefits
+        },
+        {
+          isPopular: false,
+          title: 'standard conference & expo',
+          price: 1695,
+          savings: null,
+          deadline: '',
+          benefits: benefits
+        }
+      ]
+    }
+  }
+}
+</script>
